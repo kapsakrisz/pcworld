375321d620e93f579b4692b095a4c96d7862d87f
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-02-24 08:27:00 +0000

    work02

--- src/App.js
+++ src/App.js
@@ -2,10 +2,44 @@
 
 
 const App = () => {
+
+const categories =[
+  {
+    id:1,
+    title:'Hats',
+  },
+  {
+    id:2,
+    title:'Jackets',
+  },
+  {
+    id:3,
+    title:'Sneakers',
+  },
+  {
+    id:4,
+    title:'womans',
+  },
+  {
+    id:5,
+    title:'mens',
+  }
+]
+
   return (
-    <div>
-      hello world
+    <div className="categories-container">
+    {categories.map(({title}) => (
+      <div classNAme="category-container">
+      <div className="background-image"/>
+      <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>shop now</p>
+      </div>
     </div>
+    ))}
+    
+    </div>
+    
   );
 }
 
