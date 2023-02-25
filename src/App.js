fc7956507e5c4bf42b7e495762b6b93bb7efd2b6
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-02-25 13:23:15 +0000

    styling1

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
 
-
+import Directory from './components/directory/directory.component'
 
 const App = () => {
 
 const categories =[
   {
-    id:1,
-    title:'Hats',
+    "id": 1,
+    "title": "hats",
+    "imageUrl": "https://img.overclockers.co.uk/homepage/misc/2203-hp-feature-box-cases.jpg?auto=compress%2Cformat&fit=max&q=70&w=282"
   },
   {
-    id:2,
-    title:'Jackets',
+    "id": 2,
+    "title": "jackets",
+    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
   },
   {
-    id:3,
-    title:'Sneakers',
+    "id": 3,
+    "title": "sneakers",
+    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
   },
   {
-    id:4,
-    title:'womans',
+    "id": 4,
+    "title": "womens",
+    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
   },
   {
-    id:5,
-    title:'mens',
+    "id": 5,
+    "title": "mens",
+    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
   }
-]
+];
 
   return (
-    <div className="categories-container">
-    {categories.map(({title}) => (
-      <div classNAme="category-container">
-      <div className="background-image"/>
-      <div className="category-body-container">
-      <h2>{title}</h2>
-      <p>shop now</p>
-      </div>
-    </div>
-    ))}
-    
-    </div>
+    <Directory categories={categories}/>
     
   );
 }
