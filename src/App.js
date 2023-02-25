224ef85747975f583e1e34e37a4d5720b878c2d3
krisztian kaposi <krisztiankaposi@hotmail.com>
2023-02-25 14:12:37 +0000

    changes 3

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ const App = () => {
 const categories =[
   {
     "id": 1,
-    "title": "hats",
+    "title": "Desktops",
     "imageUrl": "https://img.overclockers.co.uk/homepage/misc/2203-hp-feature-box-cases.jpg?auto=compress%2Cformat&fit=max&q=70&w=282"
   },
   {
     "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    "title": "Graphics cards",
+    "imageUrl": "https://img.overclockers.co.uk/products/OCUK-GX-1G0-GI/gam1.jpg?auto=compress%2Cformat&fit=fill&fill-color=fff&q=70&fill=solid&w=282&h=282"
   },
   {
     "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    "title": "Monitors",
+    "imageUrl": "https://img.overclockers.co.uk/products/MO-01V-MS/6-mpg-artymis-323cqr.jpg?auto=compress%2Cformat&fit=fill&fill-color=fff&q=70&fill=solid&w=282&h=282"
   },
   {
     "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    "title": "Laptops",
+    "imageUrl": "https://img.overclockers.co.uk/products/LT-2HA-AS/asuszephyrusm16.jpg?auto=compress%2Cformat&fit=fill&fill-color=fff&q=70&fill=solid&w=282&h=282"
   },
   {
     "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    "title": "GAming Chairs",
+    "imageUrl": "https://img.overclockers.co.uk/homepage/misc/2203-hp-feature-box-chairs-update.jpg?auto=compress%2Cformat&fit=max&q=70&w=282"
   }
 ];
 
